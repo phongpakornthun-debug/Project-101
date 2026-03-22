ca998a98db13fd7562349978eae6b9a4771ee754
phongpakornthun-debug <phongpakorn.thun@bumail.net>
2026-03-22 16:47:00 +0700

    LOSE UI

---
+++ Assets/Scrip/Lose UI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerLose : MonoBehaviour
+{
+    [SerializeField] private GameObject loseUI; // 👈 ลาก UI มาใส่ตรงนี้
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        loseUI.SetActive(false); // เริ่มเกมยังไม่แพ้
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("DeadZone"))
+        {
+            Lose();
+        }
+    }
+
+    void Lose()
+    {
+        loseUI.SetActive(true); // แสดง UI
+
+        Time.timeScale = 0f; // ❗ หยุดเกม
+    }
+}
